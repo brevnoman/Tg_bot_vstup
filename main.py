4a2e2f3bd1128ccbf461a7a9c7e64d01cf5000b6
brevnoman <dimasautin8@gmail.com>
2022-01-11 15:24:47 +0200

    parsing of grades and subjects

--- main.py
+++ main.py
@@ -45,7 +45,6 @@ def get_areas_dict():
             option_value = option.get("value")
             if option_value:
                 areas_dict[option_text] = f"{url}{option_value}"
-    # print(areas_dict)
     return areas_dict
 
 
@@ -68,19 +67,43 @@ def get_area_universities(area):
             uni_url = f"{uni.get('href')}"
             uni_url_sized = f"{uni_url.split('/')[2]}/"
             if uni_text not in uni_dict:
-                uni_dict[uni_text] = {"university_name": uni_text, "university_url": f"{url}{uni_url_sized}"}
+                uni_dict[uni_text] = f"{url}{uni_url_sized}"
         return uni_dict
 
     else:
         return "Wrong area. Try again"
 
 
-def get_university_department(university):
+def get_university_department(university_url):
     headers = {
         "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
         }
+    url = university_url
+    r = requests.get(url=url, headers=headers)
+    soup = BeautifulSoup(r.text, "lxml")
 
-    all_departments = get_area_universities(university)
+    deps_all = soup.select('div[class*="row no-gutters table-of-specs-item-row"]')
+    print(deps_all)
+    departments_dict = {}
+    for dep in deps_all:
+        department = dep.text.split("Факультет:")[1].split('Освітня')[0].strip()
+        speciality = dep.find("a").text
+        grades_names = dep.select('div[class*="sub"]')
+        grades_dict = {}
+        for grade in range(0, len(grades_names), 2):
+            grades_dict[grades_names[grade].text.split("(")[0]] = grades_names[grade].text.replace(" \n","").replace(")", "").replace(", ", "").replace("балmin=", "k=").split("k=")[1:3]
+        print(grades_dict)
+        if department not in departments_dict.keys():
+            departments_dict[department] = {}
+        if speciality not in departments_dict[department]:
+            departments_dict[department].setdefault(speciality, grades_dict)
 
-# if __name__ == '__main__':
-#     print(get_area_universities('м. Київ'))
+    print(departments_dict)
+
+
+
+
+
+
+if __name__ == '__main__':
+    print(get_university_department('https://vstup.osvita.ua/r27/344/'))
